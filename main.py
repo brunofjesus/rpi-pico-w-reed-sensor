36b20d2e4ae176d57ca82d73d9f92d1227e6756e
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-07-18 22:54:50 +0100

    Working on reading battery pct and deep sleep

--- main.py
+++ main.py
@@ -1,25 +1,85 @@
 import reed
 import urequests as requests
-import wlan
+from wlan import WLAN
+from ups import UPS
+import utime
+import _thread
+import lowpower
+from machine import Pin #remove
 
 use_internal_led = True
-ssid = 'NSA'
-password = '0d34c2f565'
-central = '192.168.1.85:8080'
+ssid = 'SSID'
+password = 'PASSWD'
+central = '192.168.1.100:8080'
 device_name = 'garage_door'
+debug = True
+
+seconds_until_sleep = 20
+last_action_count = seconds_until_sleep
+
+def door_handler(is_open: bool):
+    global last_action_count
+    last_action_count = seconds_until_sleep
+    
+    if internal_led:
+        internal_led.value(int(is_open))
+    
+    event = 'door_open' if is_open else 'door_closed'
+    
+    r = requests.post("http://{}/event/{}/{}".format(central,device_name,event))
+    r.close()
+
+def monitoring():
+    global last_action_count
+    
+    while True:
+        if last_action_count == 0:
+            print("going to sleep")
+            lowpower.dormant_until_pin(22)
+            print("waking up")
+            last_action_count = seconds_until_sleep
+        else:
+            last_action_count = last_action_count - 1
+        print(last_action_count)
+        utime.sleep(1)
+    
+def battery_reporter_thread(wifi):
+    try:
+        ups = UPS()
+    except OSError:
+        print("Cannot connect to UPS")
+        return
+    
+    last_ups_pct = 0
+    
+    while True:
+        stats = ups.stats()
+        print(stats)
+
+        if wifi.is_connected():            
+            if stats['p'] != last_ups_pct:
+                last_ups_pct = stats['p']
+                r = requests.post("http://{}/event/{}/battery_{}".format(central,device_name,last_ups_pct))
+                r.close()
+        else:
+            print("No connection")
+            
+        utime.sleep(60)
 
 if __name__ == "__main__":
+    # led
     internal_led = machine.Pin("LED", machine.Pin.OUT) if use_internal_led else None
+
+    # start the wifi
+    wifi = WLAN(ssid, password)
+    wifi.connect()
     
-    wlan.connect(ssid, password)
+    # start the sensor
+    sensor = reed.ReedSensor(sensor_pin_id = 22, handler=door_handler)
+
+    # monitor the UPS
+    #_thread.start_new_thread(battery_reporter_thread, (wifi,))
+    #monitoring()
     
-    def handler(is_open: bool):
-        if internal_led:
-            internal_led.value(int(is_open))
-        
-        event = 'door_open' if is_open else 'door_closed'
-            
-        r = requests.post("http://{}/event/{}/{}".format(central,device_name,event))
-        r.close() # prevent mem leak
-        
-    sensor = reed.ReedSensor(sensor_pin_id = 22, handler=handler)
+    
+ 
--- wlan.py
+++ wlan.py
@@ -1,25 +1,43 @@
 import time
 import network
 
-def connect(ssid, password):
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, password)
+class WLAN():
+    
+    def __init__(self, ssid :str, password: str):
+        self.ssid = ssid
+        self.password = password
+        self.wlan = network.WLAN(network.STA_IF)
+        
+    def active(self, enable :bool):
+        self.wlan.active(enable)
+    
+    def is_active(self):
+        return self.wlan.active()
+    
+    def ifconfig(self):
+        return self.wlan.ifconfig()
+    
+    def is_connected(self):
+        return self.wlan.isconnected()
+        
+    def connect(self):
+        self.active(True)
+        self.wlan.connect(self.ssid, self.password)
 
-    # Wait for connect or fail
-    max_wait = 10
-    while max_wait > 0:
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        max_wait -= 1
-        print('waiting for connection...')
-        time.sleep(1)
+        # Wait for connect or fail
+        max_wait = 10
+        while max_wait > 0:
+            if self.wlan.status() < 0 or self.wlan.status() >= 3:
+                break
+            max_wait -= 1
+            print('waiting for connection...')
+            time.sleep(1)
 
-    # Handle connection error
-    if wlan.status() != 3:
-        raise RuntimeError('network connection failed')
-    else:
-        print('connected')
-        status = wlan.ifconfig()
-        print( 'ip = ' + status[0] )
+        # Handle connection error
+        if self.wlan.status() != 3:
+            raise RuntimeError('network connection failed')
+        else:
+            print('connected')
+            status = self.wlan.ifconfig()
+            print( 'ip = ' + status[0] )
 
